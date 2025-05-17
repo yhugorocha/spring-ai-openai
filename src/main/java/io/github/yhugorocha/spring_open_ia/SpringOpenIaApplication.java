62f12e72ba64b03156fd42273d36422954d75225
Hugo Silva <yhugorocha@gmail.com>
2025-05-17 19:16:55 -0300

    feat: first first commit

---
+++ src/main/java/io/github/yhugorocha/spring_open_ia/SpringOpenIaApplication.java
@@ -0,0 +1,12 @@
+package io.github.yhugorocha.spring_open_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenIaApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/yhugorocha/spring_open_ia/SpringOpenIaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha.spring_open_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
